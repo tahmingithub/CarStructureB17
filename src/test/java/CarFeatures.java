6dda3192ea90fef64cf8f02eaae2d3ce5182ce9a
tahmingithub <tahminnka@gmail.com>
2023-11-04 11:10:07 -0400

    basic structure car

---
+++ src/test/java/CarFeatures.java
@@ -0,0 +1,10 @@
+public class CarFeatures {
+
+    String Doors;
+    String color;
+    String Hp;
+
+    String Model;
+}
+
+
---
+++ src/test/java/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+
+        CarFeatures audi =new CarFeatures();
+        audi.color="black";
+        audi.Hp="5000";
+        audi.Doors="2";
+        audi.Model="A90";
+    }
+
+    }
+
