cef44ab943f2a5ca298859855fee1d3d4b880669
tahmingithub <tahminnka@gmail.com>
2023-11-04 12:18:57 -0400

    added feature lights

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -6,6 +6,13 @@ public class main {
         audi.Hp="5000";
         audi.Doors="2";
         audi.Model="A90";
+
+        Lights light=new Lights();
+        light.color="blue";
+        light.opacity="50";
+        light.size="40'";
+
+
     }
 
     }
